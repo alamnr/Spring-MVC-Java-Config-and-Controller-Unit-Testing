9956f6aa40c56ba4068a09d8f78333229aa607bb
alamnr <alamnr@gmail.com>
2021-06-11 10:50:24 +0600

    jigjweang

--- src/main/java/com/spring/mvc/test/repository/AppUserRepository.java
+++ src/main/java/com/spring/mvc/test/repository/AppUserRepository.java
@@ -1,9 +1,12 @@
 package com.spring.mvc.test.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.spring.mvc.test.security.model.AppUser;
 
+
+@Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
 	public AppUser findByUserName(String username);
 }
